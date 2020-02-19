c936b6ec05720e48af74f4f5a53e7aaf9195025a
Jin Won Lee <0125jinwonlee@gmail.com>
2020-02-18 21:23:47 -0800

    Navigation part to enable logout in progress

--- api/app.js
+++ api/app.js
@@ -1,38 +1,25 @@
-import "dotenv/config";
-import express from "express";
+const createError = require("http-errors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
 
-var createError = require("http-errors");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const testAPIRouter = require("./routes/testAPI");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method");
-});
-app.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
-});
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
-});
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port" + ${process.env.PORT}!`)
-);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(helmet());
+app.use(bodyParser.json());
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
