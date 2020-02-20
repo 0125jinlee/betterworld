06236fb2e9d9269edda52fb15b52fb255cbf9820
Jin Won Lee <0125jinwonlee@gmail.com>
2020-02-19 17:01:32 -0800

    created the client folder

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
 const bodyParser = require("body-parser");
-const helmet = require("helmet");
 
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -10,7 +9,7 @@ const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const testAPIRouter = require("./routes/testAPI");
+const searchAPIRouter = require("./routes/searchAPI");
 
 const app = express();
 
@@ -18,7 +17,6 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(logger("dev"));
@@ -29,7 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
+app.use("/searchAPI", searchAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
