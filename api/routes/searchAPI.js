a7e20a372e46afac6f7f344afe6b41ad08ab6dfd
Jin Won Lee <0125jinwonlee@gmail.com>
2020-03-24 22:00:17 -0700

    searchTerm to backend server in progress

--- api/routes/searchAPI.js
+++ api/routes/searchAPI.js
@@ -3,13 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 const baseUrl =
-  "http://data.orghunter.com/v1/charitysearch?user_key=72bfc58bcc06ccc2eda85645e1ae0823&searchTerm=red";
+  "http://data.orghunter.com/v1/charitysearch?user_key=72bfc58bcc06ccc2eda85645e1ae0823&searchTerm=";
+const searchTerm = [];
 
-router.get("/", function(req, res) {
-  let searchTerm = req.searchTerm;
-  console.log(searchTerm);
+const getSearchTerm = () => {
   axios
-    .get(baseUrl)
+    .get("https://localhost:9000/searchAPI")
+    .then(res => searchTerm.push(res.data))
+    .catch(err => console.log(err.data));
+};
+
+router.get("/searchAPI", function(req, res) {
+  const url = baseUrl + searchTerm[0];
+  axios
+    .get(url)
     .then(response => {
       if (
         response.status === 200 &&
