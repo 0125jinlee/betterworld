5ccbdc9768e0d563093141608b7aa67a84c4a1a0
Jin Won Lee <0125jinwonlee@gmail.com>
2020-04-13 18:11:23 -0700

    AuthAPI call to be handle by an Express server in progress

---
+++ api/routes/authAPI.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+const express = require("express");
+const router = express.Router();
+
+let url =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCy23NFq-9Dm6qCTkocHZEr9CnpacRiBhw";
+
+if (!isSignup) {
+  url =
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCy23NFq-9Dm6qCTkocHZEr9CnpacRiBhw";
+}
+
+router.get("/", function(req, res) {
+  axios
+    .post(url, authData)
+    .then(response => {
+      const expirationDate = new Date(
+        new Date().getTime() + response.data.expiresIn * 1000
+      );
+      localStorage.setItem("token", response.data.idToken);
+      localStorage.setItem("expirationDate", expirationDate);
+      localStorage.setItem("userId", response.data.localId);
+      dispatch(
+        authActions.authSuccess(response.data.idToken, response.data.localId)
+      );
+      dispatch(checkAuthTimeout(response.data.expiresIn));
+    })
+    .catch(err => {
+      dispatch(authActions.authFail(err.response.data.error));
+    });
+});
+
+module.exports = router;
