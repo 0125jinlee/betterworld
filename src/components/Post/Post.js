ef7c8ec0ee56be9c6142f9b095e759d3adbacf93
Jin Won Lee <0125jinwonlee@gmail.com>
2020-07-23 21:56:57 -0700

    Post article buttons functionsin rprogress

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button/Button";
 
 import "./Post.css";
 
-const post = props => {
+const Post = props => {
+  const [buttonVisible, setButtonVisible] = useState(false);
+
   let category = true;
   if (props.category === "Not Provided") {
     category = false;
@@ -20,40 +23,54 @@ const post = props => {
   }
 
   return (
-    <article className="Post" onClick={props.onClick}>
-      <div>
-        <img
-          className="PostImage"
-          src={props.src}
-          alt={props.alt}
-          height="160"
-          width="330"
-        />
-        <ul>
-          <a
-            href={props.orghunterUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>{toTitleCase(props.charityName)}</h2>
-          </a>
-          <small>
-            {toTitleCase(props.city)}, {props.state}, {props.zip}
-          </small>
-        </ul>
-        <ul>
-          {category && (
-            <>
-              <span>{`${props.category} Fields`}</span>
-            </>
-          )}
-          <br></br>
-          <br></br>
-          {missionStatement ? props.missionStatement : null}
-        </ul>
-      </div>
-    </article>
+    <div className="Post">
+      <article
+        onMouseEnter={() => {
+          setButtonVisible(true);
+        }}
+        onMouseLeave={() => {
+          setButtonVisible(false);
+        }}
+        onClick={props.onClick}
+      >
+        <div className="PostButtons">
+          {buttonVisible ? <Button btnType="Website">WEBSITE</Button> : null}
+          {buttonVisible ? <Button btnType="Save">SAVE</Button> : null}
+        </div>
+        <div className="PostInfo">
+          <img
+            className="PostImage"
+            src={props.src}
+            alt={props.alt}
+            height="160"
+            width="330"
+          />
+          <ul>
+            <a
+              href={props.orghunterUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2>{toTitleCase(props.charityName)}</h2>
+            </a>
+            <small>
+              {toTitleCase(props.city)}, {props.state}, {props.zip}
+            </small>
+          </ul>
+          <ul>
+            {category && (
+              <>
+                <span>{`${props.category} Fields`}</span>
+              </>
+            )}
+            <br></br>
+            <br></br>
+            {missionStatement ? props.missionStatement : null}
+          </ul>
+        </div>
+      </article>
+    </div>
   );
 };
 
-export default post;
+export default Post;
