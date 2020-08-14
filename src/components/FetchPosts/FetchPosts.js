7a831def6d80b8f0fa673244ae4f28d437dfda66
Jin Won Lee <0125jinwonlee@gmail.com>
2020-08-13 20:18:05 -0700

    fetchPosts v1 done

--- src/components/FetchPosts/FetchPosts.js
+++ src/components/FetchPosts/FetchPosts.js
@@ -12,9 +12,48 @@ const FetchPosts = props => {
     props.onFetchPosts(props.token, props.userId);
   }, []);
 
-  let posts = <Spinner />;
-  if (!props.loading) {
-    posts = props.posts.map(post => (
+  let arr = [];
+  let base = 0;
+
+  const displayImage = () => {
+    let num = Math.floor(Math.random() * 20);
+    if (arr[num] === undefined) {
+      arr[num] = 1;
+      return num;
+    } else if (arr[num] <= base) {
+      arr[num]++;
+      return num;
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === undefined) {
+          arr[i] = 1;
+          return i;
+        } else if (arr[i] <= base) {
+          arr[i]++;
+          return i;
+        }
+      }
+      let minIndex;
+      for (let k = 0; k < arr.length; k++) {
+        if (!minIndex) {
+          minIndex = k;
+        } else if (arr[minIndex] > arr[k]) {
+          minIndex = k;
+        }
+      }
+      arr[minIndex]++;
+      base++;
+      return minIndex;
+    }
+  };
+
+  let savedPosts = <Spinner />;
+  if (
+    Array.isArray(props.posts) &&
+    props.posts.length !== 0 &&
+    !props.loading
+  ) {
+    savedPosts = props.posts.map(post => (
       <Post
         charityName={post.charityName}
         ein={post.ein}
@@ -25,16 +64,16 @@ const FetchPosts = props => {
         zip={post.zipCode}
         category={post.category}
         missionStatement={post.missionStatement}
-        // src={`/PostPictures/${displayImage() + ".jpg"}`}
+        src={`/PostPictures/${displayImage() + ".jpg"}`}
         alt={post.ein}
         key={post.ein}
       />
     ));
   }
   return (
-    <div className="SavedPosts">
+    <div className="savedPosts">
       <h1>Saved Charities</h1>
-      {posts}
+      {savedPosts}
     </div>
   );
 };
