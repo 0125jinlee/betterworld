600f561adb928e99566c5e5e7ec26bee439bb5c0
Jin Won Lee <0125jinwonlee@gmail.com>
2020-12-16 21:06:11 -0800

    minor change to savePostThunk

--- src/store/thunk/savePostThunk.js
+++ src/store/thunk/savePostThunk.js
@@ -11,22 +11,3 @@ export const savePost = (postData, uid) => {
       .set(postData);
   };
 };
-
-// firebase
-//   .database()
-//   .ref(`Users/${currentUser.uid}/posts`)
-//   .set({
-//     charityName: props.charityName,
-//     ein: props.ein,
-//     url: props.url,
-//     website: props.website,
-//     city: props.city,
-//     state: props.state,
-//     zip: props.zipCode,
-//     category: props.category,
-//     score: props.score,
-//     acceptingDonations: props.acceptingDonations,
-//     missionStatement: props.missionStatement,
-//     alt: props.ein,
-//     key: props.ein,
-//   });
